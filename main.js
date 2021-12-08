ee4f2cc022af9d8de4028907a3e1cc35a0c757aa
thinhpham199 <thinhphamtien.199@gmail.com>
2021-12-08 16:37:37 +0700

    Hello2

--- main.js
+++ main.js
@@ -8,12 +8,31 @@ function login() {
         .then(function (response) {
             console.log("Hello World");
             console.log(response);
-            alert(`Status: ${response.status}`);
-            alert(`Created: ${response.data.created}`);
-            alert(`ID: ${response.data.id}`);
-            alert(`TTL: ${response.data.ttl}`);
-            alert(`UserID: ${response.data.userId}`);
 
+            const br = document.createElement('br');
+            const newDiv = document.createElement('div');
+            newDiv.classList.add('result');
+
+            const pStatus = document.createElement('p');
+            const pCreated = document.createElement('p');
+            const pID = document.createElement('p');
+            const pTTL = document.createElement('p');
+            const pUserID = document.createElement('p');
+
+            document.getElementById("demo").appendChild(br);
+            document.getElementById("demo").appendChild(newDiv);
+
+            newDiv.appendChild(pStatus);
+            newDiv.appendChild(pCreated);
+            newDiv.appendChild(pID);
+            newDiv.appendChild(pTTL);
+            newDiv.appendChild(pUserID);
+
+            pStatus.textContent = `Status: ${response.status}`;
+            pCreated.textContent = `Created: ${response.data.created}`;
+            pID.textContent = `ID: ${response.data.id}`;
+            pTTL.textContent = `TTL: ${response.data.ttl}`;
+            pUserID.textContent = `UserID: ${response.data.userId}`
         })
         .catch(function (error) {
             console.log("̶H̶̶e̶̶l̶̶l̶̶o̶ ̶W̶̶o̶̶r̶̶l̶̶d̶");
