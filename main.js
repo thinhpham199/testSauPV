4d71005018c5524c9a39939498967230eeea1d12
thinhpham199 <thinhphamtien.199@gmail.com>
2021-12-08 14:59:31 +0700

    Test

--- main.js
+++ main.js
@@ -8,6 +8,12 @@ function login() {
         .then(function (response) {
             console.log("Hello World");
             console.log(response);
+            alert(`Status: ${response.status}`);
+            alert(`Created: ${response.data.created}`);
+            alert(`ID: ${response.data.id}`);
+            alert(`TTL: ${response.data.ttl}`);
+            alert(`UserID: ${response.data.userId}`);
+
         })
         .catch(function (error) {
             console.log("̶H̶̶e̶̶l̶̶l̶̶o̶ ̶W̶̶o̶̶r̶̶l̶̶d̶");
